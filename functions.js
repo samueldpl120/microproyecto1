65912ceed2425fcede234477d006554470fe4d5c
samueldpl120 <97692414+samueldpl120@users.noreply.github.com>
2024-02-11 20:07:29 -0400

    Commit 2

--- functions.js
+++ functions.js
@@ -1,36 +1,35 @@
-var turno = 0;
+var turno = 1;
 var indexJugador = 0;
+var numerosObtenidos = [];
 
 //Arreglo con los 4 objetos que representan a los jugadores
-let jugadores = [JSON.parse(sessionStorage.getItem('jugador1')),JSON.parse(sessionStorage.getItem('jugador2')), JSON.parse(sessionStorage.getItem('jugador3')), JSON.parse(sessionStorage.getItem('jugador4'))]
+let jugadores = [
+  JSON.parse(sessionStorage.getItem("jugador1")),
+  JSON.parse(sessionStorage.getItem("jugador2")),
+  JSON.parse(sessionStorage.getItem("jugador3")),
+  JSON.parse(sessionStorage.getItem("jugador4")),
+];
 
 var idturno = document.getElementById("turnos");
 idturno.textContent = turno;
 
 document.addEventListener("DOMContentLoaded", function () {
+
   let jugador1 = JSON.parse(sessionStorage.getItem("jugador1"));
-  console.log(jugador1)
+  console.log(jugador1);
 
-  //Crear arreglo que contendrá los numeros que ya hayan salido
-  let numeros = [];
-  sessionStorage.setItem("numerosSacados", JSON.stringify(numeros));
-
-  // Mostrar los valores en la página
   var nombreJugador = document.getElementById("jugador");
   nombreJugador.textContent = jugador1.nombre;
 
-  // Acceder al atributo 'matriz' del objeto j1
   let matriz = jugador1.matriz;
-  console.log(matriz)
+  console.log(matriz);
 
   imprimirMatriz(matriz);
 });
 
-//Funcion para generar una matriz desde 0
 function imprimirMatriz(matriz) {
   let carton = document.getElementById("carton");
   const tabla = document.createElement("table");
-  let arreglo = JSON.parse(sessionStorage.getItem("numerosSacados"));
 
   for (let i = 0; i < matriz.length; i++) {
     let fila = document.createElement("tr");
@@ -38,7 +37,7 @@ function imprimirMatriz(matriz) {
     for (let j = 0; j < matriz[i].length; j++) {
       let celda = document.createElement("td");
       celda.textContent = matriz[i][j];
-      if (arreglo.includes(parseInt(celda.textContent))) {
+      if (numerosObtenidos.includes(parseInt(celda.textContent))) {
         celda.style.backgroundColor = "red";
       }
       fila.appendChild(celda);
@@ -48,15 +47,13 @@ function imprimirMatriz(matriz) {
   carton.appendChild(tabla);
 }
 
-
 function ModificarMatriz(matriz) {
   const carton = document.getElementById("carton");
   while (carton.firstChild) {
-      carton.removeChild(carton.firstChild);
+    carton.removeChild(carton.firstChild);
   }
   imprimirMatriz(matriz);
 }
-
 
 function siguiente() {
   indexJugador = indexJugador + 1;
@@ -71,7 +68,6 @@ function siguiente() {
   ModificarMatriz(jugador.matriz);
 }
 
-
 function Actual() {
   let jugador = jugadores[indexJugador];
   elementact = JSON.parse(elementoActual);
@@ -79,7 +75,6 @@ function Actual() {
   document.getElementById("jugador").textContent = jugador.nombre;
   ModificarMatriz(jugador.matriz);
 }
-
 
 function anterior() {
   indexJugador = indexJugador - 1;
@@ -97,4 +92,65 @@ function anterior() {
 function salir() {
   sessionStorage.clear();
   window.location.href = "index.html";
+}
+
+function mostrarNumero() {
+  var numeroAleatorio = Math.floor(Math.random() * 50) + 1;
+  console.log(numeroAleatorio);
+  if (!numerosObtenidos.includes(numeroAleatorio)) {
+    numerosObtenidos.push(numeroAleatorio);
+    let numero = document.getElementById("numero");
+    numero.textContent = numeroAleatorio;
+  } else {
+    while (numerosObtenidos.includes(numeroAleatorio)) {
+      var numeroAleatorio = Math.floor(Math.random() * 50) + 1;
+    }
+    numerosObtenidos.push(numeroAleatorio);
+    let numero = document.getElementById("numero");
+    numero.textContent = numeroAleatorio;
+  }
+  document.getElementById("popUp").style.display = "block";
+}
+
+function cerrarPopUp() {
+  document.getElementById("popUp").style.display = "none";
+  turno = turno + 1;
+  var idturno = document.getElementById("turnos");
+  idturno.textContent = turno;
+}
+
+function filas(matriz, arreglo) {
+  var filasCompletas = 0;
+
+  matriz.forEach(function(fila) {
+    var filaEnArreglo = fila.every(function(elemento) {
+      return arreglo.includes(elemento);
+    });
+
+    if (filaEnArreglo) {
+      filasCompletas++;
+    }
+  });
+
+  return filasCompletas;
+}
+
+function columnas(matriz, arreglo) {
+  return matriz[0].reduce((acum, _, columnIndex) => {
+    const columnaCompleta = matriz.every(row => arreglo.includes(row[columnIndex]));
+    return acum + (columnaCompleta ? 1 : 0);
+  }, 0);
+}
+
+function diagonales(matriz, arreglo) {
+  const diagonalPrincipalContenida = matriz.every((row, i) => arreglo.includes(row[i]));
+  const diagonalSecundariaContenida = matriz.every((row, i) => arreglo.includes(row[matriz.length - 1 - i]));
+  let suma = 0;
+  if (diagonalPrincipalContenida) suma += 2;
+  if (diagonalSecundariaContenida) suma += 2;
+  return suma;
+}
+
+function CartonLleno(matriz, arreglo) {
+  return matriz.flat().every(numero => arreglo.includes(numero));
 }
